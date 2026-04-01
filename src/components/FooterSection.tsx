11e38cbbcbaea0e15a17755c8ffb85faba1e78e7
darkeris345 <dimuxxx72@gmail.com>
2026-04-01 21:49:38 +0300

    Footer update

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,9 +1,9 @@
 const FooterSection = () => {
   return (
-    <footer className="border-t border-border py-12">
+    <footer className=" border-border py-1">
       <div className="container mx-auto px-6 flex items-center justify-center">
         <p className="text-muted-foreground text-sm font-light">
-          © {new Date().getFullYear()} VšĮ „Tėvynės labui". All rights reserved.
+          © {new Date().getFullYear()} VšĮ „Tėvynės labui"
         </p>
       </div>
     </footer>
