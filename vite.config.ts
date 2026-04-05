d0f84f358ddc725175016709c5b7125616851195
darkeris345 <dimuxxx72@gmail.com>
2026-04-05 19:08:35 +0300

    added vite-image-optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { ViteImageOptimizer  } from "vite-plugin-image-optimizer";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,7 +11,25 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer ({
+      exclude: ['**/hero-platoon.png'],
+      include: ['**/*.{png,jpg,jpeg,gif,svg,webp}'],
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
